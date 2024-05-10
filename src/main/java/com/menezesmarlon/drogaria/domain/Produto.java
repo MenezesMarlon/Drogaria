e948177cd8455fbd3761ccbd40efdd767be05323
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-05-10 20:34:09 -0300

    Implementação Lombok

--- src/main/java/com/menezesmarlon/drogaria/domain/Produto.java
+++ src/main/java/com/menezesmarlon/drogaria/domain/Produto.java
@@ -1,12 +1,16 @@
 package com.menezesmarlon.drogaria.domain;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Produto {
 
     // Os atributos de uma classe correspondem às colunas de uma tabela, AVALIE BEM!
