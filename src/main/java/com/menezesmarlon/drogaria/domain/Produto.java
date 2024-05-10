c09dbda728a0d4254dd3be7cf8e254fc83040224
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-05-10 20:28:32 -0300

    Correção Sintaxes

--- src/main/java/com/menezesmarlon/drogaria/domain/Produto.java
+++ src/main/java/com/menezesmarlon/drogaria/domain/Produto.java
@@ -1,14 +1,18 @@
 package com.menezesmarlon.drogaria.domain;
 
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Data
 public class Produto {
+
     // Os atributos de uma classe correspondem às colunas de uma tabela, AVALIE BEM!
     private Short codigo;
     private String nome;
     private Byte quantidade;
-    private BigDecimal preCo;
-    private LocalDate validade;
+    private BigDecimal preco;
+    private LocalDate dataValidade;
 }
