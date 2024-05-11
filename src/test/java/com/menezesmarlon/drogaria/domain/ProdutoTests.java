15a213d2135d5f897236fe6ca8d5cef8883fb3d9
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-05-10 21:02:26 -0300

    Criação dos testes unitários para a classe Produto

---
+++ src/test/java/com/menezesmarlon/drogaria/domain/ProdutoTests.java
@@ -0,0 +1,22 @@
+package com.menezesmarlon.drogaria.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@SpringBootTest // carrega o contexto do spring
+public class ProdutoTests {
+    @Test
+    public void criar() {
+        Produto p = new Produto(
+                Short.valueOf("1"),
+                "coca cola",
+                Byte.valueOf("15"),
+                BigDecimal.valueOf(10.50),
+                LocalDate.of(2024, 12, 30)
+        );
+        System.out.println(p);
+    }
+}
