850d2d2d48a2c1fb87ef3981563368e39b4b6066
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-05-17 14:25:54 -0300

    Criação Repository Categoria

---
+++ src/main/java/com/menezesmarlon/drogaria/repository/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package com.menezesmarlon.drogaria.repository;
+
+import com.menezesmarlon.drogaria.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Byte> {
+
+}
