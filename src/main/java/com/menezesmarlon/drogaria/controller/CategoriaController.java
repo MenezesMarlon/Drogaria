4daf6d16e5777920d499cd9fea30a3a30fd7b0e0
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-05-19 16:57:44 -0300

    Criação metodo de inserção de categorias

--- src/main/java/com/menezesmarlon/drogaria/controller/CategoriaController.java
+++ src/main/java/com/menezesmarlon/drogaria/controller/CategoriaController.java
@@ -3,9 +3,7 @@ package com.menezesmarlon.drogaria.controller;
 import com.menezesmarlon.drogaria.domain.Categoria;
 import com.menezesmarlon.drogaria.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,11 @@ public class CategoriaController {
     public List<Categoria> listar() {
         List<Categoria> categorias = categoriaRepository.findAll();
         return categorias;
+    }
+    @PostMapping
+    public Categoria inserir(@RequestBody Categoria categoria){
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+        return categoriaSalva;
     }
 
 }
