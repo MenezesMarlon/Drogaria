ee77f27de72fac7fe50fd73329b3a9cab6eb7ef6
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-05-20 16:21:19 -0300

    Criação de CategoriaTests

---
+++ src/test/java/com/menezesmarlon/drogaria/domain/CategoriaTests.java
@@ -0,0 +1,14 @@
+package com.menezesmarlon.drogaria.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CategoriaTests {
+//    @Test
+//    public void criar(){
+//        Categoria c = new Categoria(
+//                Integer.valueOf("1"),"Remedio");
+//        System.out.println(c);
+//    }
+}
