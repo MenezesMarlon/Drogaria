b9d3cb21553544653841b2b87c1cbde43f36d8a7
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-05-20 16:22:40 -0300

    Update ProdutoTests

--- src/test/java/com/menezesmarlon/drogaria/domain/ProdutoTests.java
+++ src/test/java/com/menezesmarlon/drogaria/domain/ProdutoTests.java
@@ -8,16 +8,16 @@ import java.time.LocalDate;
 
 @SpringBootTest // carrega o contexto do spring
 public class ProdutoTests {
-    @Test
-    public void criar() {
-        Produto p = new Produto(
-                Short.valueOf("1"),
-                "coca cola",
-                Byte.valueOf("15"),
-                BigDecimal.valueOf(10.50),
-                LocalDate.of(2024, 12, 30),
-                null
-        );
-        System.out.println(p);
-    }
+//    @Test
+//    public void criar() {
+//        Produto p = new Produto(
+//                Short.valueOf("1"),
+//                "coca cola",
+//                Byte.valueOf("15"),
+//                BigDecimal.valueOf(10.50),
+//                LocalDate.of(2024, 12, 30),
+//                null
+//        );
+//        System.out.println(p);
+//    }
 }
