4f2bad1104ad650f7d40bca0e6022c6b731e87c9
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-05-21 16:44:31 -0300

    Criação metodo de Busca por Codigo

--- src/main/java/com/menezesmarlon/drogaria/controller/CategoriaController.java
+++ src/main/java/com/menezesmarlon/drogaria/controller/CategoriaController.java
@@ -17,25 +17,32 @@ public class CategoriaController {
 
     @GetMapping
     public List<Categoria> listar() {
-        List<Categoria> categorias = categoriaRepository.findAll(Sort.by(Sort.Direction.ASC,"codigo"));
+        List<Categoria> categorias = categoriaRepository.findAll(Sort.by(Sort.Direction.ASC, "codigo"));
         return categorias;
     }
+
+    @GetMapping("/{codigo}")
+    public Categoria buscarPorCodigo(@PathVariable Integer codigo) {
+        return categoriaRepository.findById(codigo).get();
+    }
+
     @PostMapping
-    public Categoria inserir(@RequestBody Categoria categoria){
+    public Categoria inserir(@RequestBody Categoria categoria) {
         Categoria categoriaSalva = categoriaRepository.save(categoria);
         return categoriaSalva;
     }
 
     @DeleteMapping("/{codigo}")
-    public Categoria remover(@PathVariable Integer codigo){
+    public Categoria remover(@PathVariable Integer codigo) {
         Categoria categoria = categoriaRepository.findById(codigo).get();
         categoriaRepository.delete(categoria);
         return categoria;
     }
 
     @PutMapping
-    public Categoria editar(@RequestBody Categoria categoria){
+    public Categoria editar(@RequestBody Categoria categoria) {
         return categoriaRepository.save(categoria);
     }
+
 
 }
