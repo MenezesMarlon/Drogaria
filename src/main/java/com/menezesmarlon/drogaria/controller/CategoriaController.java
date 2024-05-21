8e1731cdfc34b8f6440e429a1255613af1b4597e
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-05-21 17:49:36 -0300

    Implementação tratativa de erros

--- src/main/java/com/menezesmarlon/drogaria/controller/CategoriaController.java
+++ src/main/java/com/menezesmarlon/drogaria/controller/CategoriaController.java
@@ -1,9 +1,13 @@
 package com.menezesmarlon.drogaria.controller;
 
 import com.menezesmarlon.drogaria.domain.Categoria;
+import com.menezesmarlon.drogaria.exception.ResourceNotFindException;
 import com.menezesmarlon.drogaria.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
 
 @RestController
@@ -19,8 +23,12 @@ public class CategoriaController {
     }
     @GetMapping("/{codigo}")
     public Categoria buscaPorCodigo(@PathVariable Integer codigo){
+        try{
         Categoria categoria = categoriaService.buscarPorCodigo(codigo);
         return categoria;
+        } catch (ResourceNotFindException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada", e);
+        }
     }
     @PostMapping
     public Categoria inserir(@RequestBody Categoria categoria) {
