1208cf47257d56dd63a4ded7f399d45a363c91a4
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-05-22 12:03:04 -0300

    Implementação dos metodos de Listagem

--- src/main/java/com/menezesmarlon/drogaria/controller/ProdutoController.java
+++ src/main/java/com/menezesmarlon/drogaria/controller/ProdutoController.java
@@ -1,13 +1,28 @@
 package com.menezesmarlon.drogaria.controller;
 
+import com.menezesmarlon.drogaria.domain.Produto;
 import com.menezesmarlon.drogaria.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
+
+    @GetMapping
+    public List<Produto> listar() {
+        List<Produto> produtos = produtoService.listar();
+        return produtos;
+    }
+
+    @GetMapping("/{codigo}")
+    public Produto buscaPorCodigo(@PathVariable Short codigo) {
+        Produto produto = produtoService.buscaPorCodigo(codigo);
+        return produto;
+    }
+
 }
--- src/main/java/com/menezesmarlon/drogaria/service/ProdutoService.java
+++ src/main/java/com/menezesmarlon/drogaria/service/ProdutoService.java
@@ -1,11 +1,24 @@
 package com.menezesmarlon.drogaria.service;
 
+import com.menezesmarlon.drogaria.domain.Produto;
 import com.menezesmarlon.drogaria.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
+
+    public List<Produto> listar() {
+        List<Produto> produtos = produtoRepository.findAll();
+        return produtos;
+    }
+
+    public Produto buscaPorCodigo(Short codigo) {
+        Produto produto = produtoRepository.findById(codigo).get();
+        return produto;
+    }
 }
